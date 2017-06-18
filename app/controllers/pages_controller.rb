45f5e4d1628eaac77cf73f7ce2afcd3a53cb439f
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-06-18 13:04:21 -0400

    try to put in devise controllers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,15 +11,15 @@ class PagesController < ApplicationController
     end
     # SEARCH BY CURRENT USER FIRST
     # LET USERS DECIDE HOW FAR OUT WE GO FOR DATES
-    pocket_money_expenses = Expense.where("user_id => ? AND date > ? AND date < ?", current_user.id, Time.now, (Time.now + 7.weeks))
-    @pocket_money = current_user.last_balance
-    pocket_money_expenses.each do |expense|
-      @pocket_money << expense.amount.to_d
-    end
-    pocket_money_event = Event.where("user_id => ? AND date > ? AND date < ?", current_user.id, Time.now, (Time.now + 7.weeks))
-    pocket_money_event.each do |event|
-      @pocket_money << event.amount.to_d
-    end
+    # pocket_money_expenses = Expense.where("user_id => ? AND date > ? AND date < ?", current_user.id, Time.now, (Time.now + 7.weeks))
+    # @pocket_money = current_user.last_balance
+    # pocket_money_expenses.each do |expense|
+    #   @pocket_money << expense.amount.to_d
+    # end
+    # pocket_money_event = Event.where("user_id => ? AND date > ? AND date < ?", current_user.id, Time.now, (Time.now + 7.weeks))
+    # pocket_money_event.each do |event|
+    #   @pocket_money << event.amount.to_d
+    # end
   end
 
 end
