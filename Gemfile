68c03fe41d047e3e0f9165629bf6c16c91fce317
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-09-01 16:12:40 -0700

    added api keys

--- Gemfile
+++ Gemfile
@@ -52,6 +52,7 @@ group :development, :test do
     gem 'rspec-rails', '~> 3.5'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'dotenv-rails'
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  # before_action :restrict_access
 
   def update_pocket
     if current_user
       current_user.account.pocket_money_update
+    end
+  end
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |api_key, options|
+      User.find_by(email: request.headers['X-User-Email'], api_key: api_key)
     end
   end
 end
